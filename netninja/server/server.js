f20a16b92a8b977669c5b50b6fb35c34ffbf8771
xaositectt <tamas.varga.420@gmail.com>
2018-07-21 02:08:56 +0200

    worked on stream examples

--- netninja/server/server.js
+++ netninja/server/server.js
@@ -2,17 +2,32 @@
 
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  console.log('reqest was made: ' + req.url);
-  res.writeHead(200, {
-    'Content-Type': 'text/plain',
-    'name': 'Paige',
-    'say': 'What',
-  }
-);
-  res.end('hello there');
+const fs = require('fs');
+
+//create a readable stream
+//without utf8 enconding it just dosplays as numbers
+const readStream = fs.createReadStream(__dirname + '/source.txt', 'utf8');
+
+const writeStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+
+
+//write to file with the method
+readStream.on('data', (chunk) => {
+  console.log('new chunk received');
+  writeStream.write(chunk);
 });
 
-server.listen(3000, '127.0.0.1', () => {
-  console.log('listening to port 3000');
-});
+// const server = http.createServer((req, res) => {
+//   console.log('reqest was made: ' + req.url);
+//   res.writeHead(200, {
+//     'Content-Type': 'text/plain',
+//     'name': 'Paige',
+//     'say': 'What',
+//   }
+// );
+//   res.end('hello there');
+// });
+
+// server.listen(3000, '127.0.0.1', () => {
+//   console.log('listening to port 3000');
+// });
